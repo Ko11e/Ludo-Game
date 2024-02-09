c0aec678a4204f8d4f95dbfc0c5069b0e8934aea
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-02-09 09:49:12 +0000

    JS: Make buttons interactive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "startGame") {
+                alert("You have not yet selected the number of players");
+            } else {
+                let numberPlayers = this.getAttribute("data-type");
+                alert(`You have selected ${numberPlayers}`);
+            }
+        });
+    }
+})
+
+
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -17,9 +35,10 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// 
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+

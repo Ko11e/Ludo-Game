cc2d962553f9b9935891f03b2b5c1ce927accf9c
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-02-13 14:06:28 +0000

    JS: Add positions to the pawns

--- assets/js/script_board.js
+++ assets/js/script_board.js
@@ -47,32 +47,9 @@ function getPlayerNameFromURL() {
 function createBoard(Listplayers) {
 
     let listLenght = Listplayers.length;
-    let colors = [[['red1', 0], ['red2', 0], ['red3', 0], ['red4', 0]], 
-    [['yellow1', 0], ['yellow2', 0], ['yellow3', 0], ['yellow4', 0]], 
-    [['green1', 0], ['green2', 0],['green3', 0],['green4', 0]], 
-    [['blue1', 0], ['blue2', 0],['blue3', 0],['blue4', 0]]];
-    let statPlayers = new Array();
-    let pathways = [['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', 
-    '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '1.1', '1.2', '1.3', '1.4'],
-    ['11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', 
-    '32', '33', '34', '35', '36', '37', '38', '39', '40', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '2.1', '2.2', '2.3', '2.4'],
-    ['21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '1', 
-    '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '3.1', '3.2', '3.3', '3.4'],
-    ['31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', 
-    '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '4.1', '4.2', '4.3', '4.4']]
 
     removenonplayers(listLenght);
-
-    for (let i = 0; i < listLenght; i++) {
-        let nameText = document.getElementById(`playerText${i+1}`);
-        console.log(Listplayers[i]);
-        //if (Listplayers[i] === '');
-            //Listplayers[i] = `Player${i+1}`;
-        nameText.innerHTML = Listplayers[i];
-        let stat = {Name: Listplayers[i], Nest: 0, Pathway: pathways[i], Pawn: colors[i]};
-        statPlayers.push(stat);
-    }
-    console.log(statPlayers);
+    let statPlayers = createplayerobject(Listplayers)
 
     return statPlayers;
 }
@@ -90,18 +67,48 @@ function removenonplayers(Numberofplayers) {
     }
 }
 
+function createplayerobject(array){
+    let listLenght = array.length;
+    let colors = [[['red1', 'homeRed1'], ['red2', 'homeRed2'], ['red3', 'homeRed3'], ['red4', 'homeRed4']], 
+    [['yellow1', 'homeYellow1'], ['yellow2', 'homeYellow2'], ['yellow3', 'homeYellow3'], ['yellow4', 'homeYellow4']], 
+    [['green1', 'homeGreen1'], ['green2', 'homeGreen2'],['green3', 'homeGreen3'],['green4', 'homeGreen4']], 
+    [['blue1', 'homeBlue1'], ['blue2', 'homeBlue2'],['blue3', 'homeBlue3'],['blue4', 'homeBlue4']]];
+    let statPlayers = new Array();
+    let pathways = [['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', 
+    '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '1.1', '1.2', '1.3', '1.4'],
+    ['11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', 
+    '32', '33', '34', '35', '36', '37', '38', '39', '40', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '2.1', '2.2', '2.3', '2.4'],
+    ['21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '1', 
+    '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '3.1', '3.2', '3.3', '3.4'],
+    ['31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', 
+    '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '4.1', '4.2', '4.3', '4.4']]
+    
+    for (let i = 0; i < listLenght; i++) {
+        let nameText = document.getElementById(`playerText${i+1}`);
+
+        //if (Listplayers[i] === '');
+            //Listplayers[i] = `Player${i+1}`;
+        nameText.innerHTML = array[i];
+        let stat = {Name: array[i], Nest: 0, Pathway: pathways[i], Pawn: colors[i]};
+        statPlayers.push(stat);
+    }
+
+    return statPlayers
+
+}
+
 function firstTurn(ArrayPlayers) {
     for (let i=0; i<ArrayPlayers.length; i++){
-            let pathWay = ArrayPlayers[i].Pathway;
-            let pawns = ArrayPlayers[i].Pawn;
-            let rolls = 0;
-            if (rolls !== 0){
-                dice = rollDice();
-                if (dice === 1);                
-                    let pawn = document.getElementById(paws[0][0])
-                    let pathpalate = 1;
-                    document.getElementById('pawn').appendChild(document.getElementById('trinket1'));
-            }
+        let pathWay = ArrayPlayers[i].Pathway;
+        let pawns = ArrayPlayers[i].Pawn;
+        let rolls = 0;
+        if (rolls !== 0){
+            dice = rollDice();
+            if (dice === 1);                
+                let pawn = document.getElementById(paws[0][0])
+                document.getElementById('pawn');
+                document.getElementById('trinket1'));
+        }
 
 
     }
